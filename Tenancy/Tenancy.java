f397fc66d16d81576b7bf0060e57d83c8435351f
Davi Oliveira <davi.comp.oliveira@gmail.com>
2022-01-25 13:55:34 -0300

    adicionado esqueleto da classe tenancy

--- Tenancy/Tenancy.java
+++ Tenancy/Tenancy.java
@@ -1,5 +1,56 @@
 package Tenancy;
 
+import Date.Date;
+import Vehicles.*;
+import java.util.List;
+import java.util.ArrayList;
+
 public class Tenancy {
-  
+  private int id;
+  private Date rentDate;
+  private Date devolutionDate;
+  private String client;
+  private boolean status;
+  private List<Car> cars = new ArrayList<Car>();
+  private List<Motorcycle> motorcycles = new ArrayList<Motorcycle>();
+
+  Tenancy() {
+
+  }
+
+  public float calculateFine() {
+    return 0;
+  }
+
+  public float calculateRentValue() {
+    return 0;
+  }
+
+  public void setStatus() {
+
+  }
+
+  public boolean verifyStatus() {
+    return status;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public Date getRentDate() {
+    return rentDate;
+  }
+
+  public Date getDevolutionDate() {
+    return devolutionDate;
+  }
+
+  public String getClient() {
+    return client;
+  }
+
+  public String toString() {
+    return (cars + " " + motorcycles);
+  }
 }
